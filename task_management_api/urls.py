0a91b60fa79af1b55b988aa9b0cf795dc5f9a5f5
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-28 11:35:06 +0330

    swagger was added to help us examine the apis

--- task_management_api/urls.py
+++ task_management_api/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .swagger import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
