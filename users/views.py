29ea20fc8aa98f0119fbdd038b34064e2ca64fcf
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-28 15:40:30 +0330

    register view was completed

--- users/views.py
+++ users/views.py
@@ -2,14 +2,26 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from users.serializers import UserSerializer
+from users.serializers import RegisterSerializer
+from users.models import CustomUser
 
 
 class RegisterView(APIView):
 
     def post(self, request):
-        serializer = UserSerializer(date=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+        serializer = RegisterSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'user': serializer.data, 'token': token.key}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+{ 
+   "username": "narges",
+    "email": "narges@gmail.com",
+    "password": "12345678",
+    "confirm_password": "12345678"
+}
