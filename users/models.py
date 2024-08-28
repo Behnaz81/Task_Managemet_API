5135fd9ffa2b26142f9040b51a33c19e39792d70
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-28 13:50:59 +0330

    CustomUser model and Team models were created

--- users/models.py
+++ users/models.py
@@ -1,8 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 
 class Team(models.Model):
 
     title = models.CharField(max_length=150)
 
 
+class CustomUser(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('teammember', 'Team Member'),
+        ('teamleader', 'Team Leader'),
+        ('manager', 'Manager'),
+    )
+
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+
+    
