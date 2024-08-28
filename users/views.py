b76eacafe5de8a5824bcdc9cc53cbaf4d4c042f2
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-28 14:25:54 +0330

    register view was created

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(date=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
