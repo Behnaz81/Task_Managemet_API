319ed5b73f668e0a17e2df1e0545eccaf7f7b14d
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-28 15:39:58 +0330

    create and validate methods were added to the serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,32 @@
-from rest_framework.serializers import ModelSerializer
-from users.models import CustomUser
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
-class UserSerializer(ModelSerializer):
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
     class Meta:
-        model = CustomUser
-        fields = ['username', 'password', 'email']
+        model = User
+        fields = ['username', 'email', 'password', 'confirm_password']
+
+
+    def validate(self, data):
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop('confirm_password', None)
+        
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
