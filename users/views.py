22df858a383e903a6400c672d91fa58aa82d629b
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-28 15:54:21 +0330

    Login users is now possible

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
 from users.serializers import RegisterSerializer
 from users.models import CustomUser
 
@@ -19,9 +21,15 @@ class RegisterView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-{ 
-   "username": "narges",
-    "email": "narges@gmail.com",
-    "password": "12345678",
-    "confirm_password": "12345678"
-}
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'details':'Invalid data'}, status=status.HTTP_401_UNAUTHORIZED)
+
