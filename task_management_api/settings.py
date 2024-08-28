ea188a667ca2b7b441fd8fbab417f509831fd734
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-28 19:17:15 +0330

    settings were updated for the rest framework

--- task_management_api/settings.py
+++ task_management_api/settings.py
@@ -130,3 +130,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+       'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
+
