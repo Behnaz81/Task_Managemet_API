436a061a5ef980e7b5401324692b5fd8d061d872
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-30 10:33:16 +0330

    Project model was defined

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from users.models import Team
 
-# Create your models here.
+User = get_user_model()
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    team_id = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null= True)
