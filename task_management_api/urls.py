536db111728bce0b3c1ace202ef67280acf94855
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-30 10:35:26 +0330

    projects urls are included in the main project

--- task_management_api/urls.py
+++ task_management_api/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/users/', include('users.urls')),
+    path('api/projects/', include('projects.urls')),
 ]
