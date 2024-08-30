e90396ebe1074ec18112365d72b81f06d18f7294
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-30 11:14:06 +0330

    a url was assigned to project assignments

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from projects.views import CreateProjectView
+from projects.views import CreateProjectView, AssignProjectView
 
 urlpatterns = [
     path('create-project/', CreateProjectView.as_view(), name="creat-project"),
+    path('assign-project/<int:id>/', AssignProjectView.as_view(), name="assign-project"),
 ]
