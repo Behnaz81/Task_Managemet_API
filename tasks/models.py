7b002e0c69af5756e3899705c48cbdf23e6029e9
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-30 17:20:52 +0330

    related name field was added to the foreign keys

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from projects.models import Project
 
-# Create your models here.
+User = get_user_model()
+
+class Task(models.Model):
+    class Priority(models.IntegerChoices):
+        VERY_HIGH = 1, "Very Important"
+        HIGH = 2, "Important"
+        MEDIUM = 3, "Medium"
+        LOW = 4, "Low"
+        VERY_LOW = 5, "Very Low"
+
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='assigned_to')
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='created_by')
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    priority = models.IntegerField(choices=Priority)
+    due_date = models.DateField()
+    
