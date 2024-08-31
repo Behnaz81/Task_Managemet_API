7f2d5480d57f45faca71c2d22469889137963ea6
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-31 14:01:50 +0330

    CustomUser model is updated. each member will be assigned a team

--- users/models.py
+++ users/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
+
+class Team(models.Model):
+
+    title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+    
 
 class CustomUser(AbstractUser):
 
@@ -10,19 +19,5 @@ class CustomUser(AbstractUser):
         ('manager', 'Manager'),
     )
 
-    role = models.CharField(max_length=20, default='teammember',choices=ROLE_CHOICES)
-
-
-class Team(models.Model):
-
-    title = models.CharField(max_length=150)
-    created_by = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-
-
-class TeamMembers(models.Model):
-
-    team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
-    member_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, default='teammember', choices=ROLE_CHOICES)
+    team = models.ManyToManyField(Team)
