ece848225bd89743e8efbb66cfba097592d1f97a
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-31 14:39:31 +0330

    TeamMembership model was added

--- users/models.py
+++ users/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
 
 
+ROLE_CHOICES = (
+        ('teammember', 'Team Member'),
+        ('teamleader', 'Team Leader'),
+        ('manager', 'Manager'),
+    )
+
+
 class Team(models.Model):
 
     title = models.CharField(max_length=150)
@@ -13,11 +20,26 @@ class Team(models.Model):
 
 class CustomUser(AbstractUser):
 
-    ROLE_CHOICES = (
-        ('teammember', 'Team Member'),
-        ('teamleader', 'Team Leader'),
-        ('manager', 'Manager'),
-    )
-
     role = models.CharField(max_length=20, default='teammember', choices=ROLE_CHOICES)
-    team = models.ManyToManyField(Team)
+    team = models.ManyToManyField(Team, through='TeamMembership') 
+
+
+class TeamMembership(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    joined_at = models.DateField(auto_now_add=True) 
+    role_within_team = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('user', 'team') 
+
+    
+    def save(self, *args, **kwargs):
+        if not self.role_within_team:
+            self.role_within_team = self.user.role
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user.username} in {self.team.title} as {self.role_within_team}"
+    
