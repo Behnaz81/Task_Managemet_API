8b14e01be447766fcef69bb8c88a2f5f9716a402
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-31 15:14:34 +0330

    TeamMembership model was registered in the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from users.models import CustomUser, Team
+from users.models import CustomUser, Team, TeamMembership
 
 admin.site.register(CustomUser)
 admin.site.register(Team)
+admin.site.register(TeamMembership)
