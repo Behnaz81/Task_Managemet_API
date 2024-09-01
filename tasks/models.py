15671f28c9157b8e7775f82eebd364191e07530f
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-01 10:51:22 +0330

    Task model was defined

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} - Due to {self.due_date}"
